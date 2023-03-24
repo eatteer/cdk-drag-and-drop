ed1d77f3343f1357c82c7217699aeb0ef2722629
Practicante Programacion TI <practicante.ti2@austinreed.com.co>
2023-03-24 17:02:26 -0500

    Upload repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
+
+export interface Board {
+  lists: List[];
+}
+
+export interface List {
+  title: string;
+  tasks: Task[];
+}
+
+export interface Task {
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cdk-drag-and-drop';
+  public board: Board = {
+    lists: [
+      {
+        title: 'Todo',
+        tasks: [{ text: 'Do something' }, { text: 'Do another thing' }],
+      },
+      { title: 'In Progress', tasks: [{ text: 'Make my hair' }] },
+      {
+        title: 'Done',
+        tasks: [
+          { text: 'Apply to that job' },
+          { text: 'Learn more Angular' },
+          { text: 'Learn more CSS' },
+        ],
+      },
+    ],
+  };
+
+  public drop(event: CdkDragDrop<List>): void {
+    const { previousIndex, currentIndex, previousContainer, container } = event;
+    const isSamePosition = previousIndex === currentIndex;
+    const isSameContainer = previousContainer === container;
+
+    if (isSameContainer && isSamePosition) return;
+
+    isSameContainer
+      ? moveItemInArray(container.data.tasks, previousIndex, currentIndex)
+      : transferArrayItem(
+          previousContainer.data.tasks,
+          container.data.tasks,
+          previousIndex,
+          currentIndex
+        );
+  }
 }
